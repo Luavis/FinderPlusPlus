3be5eb48212574cb4a2f788a56115a92104a8bf2
Luavis <luaviskang@gmail.com>
2016-06-23 16:03:33 +0900

    Initial Commit

---
+++ FinderPlusPlus/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  FinderPlusPlus
+//
+//  Created by Luavis on 6/23/16.
+//  Copyright © 2016 Luavis. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
