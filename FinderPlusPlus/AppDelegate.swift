47208c0060ef6ecffd36d8178a7e614a67e01a5e
Luavis <luaviskang@gmail.com>
2016-07-15 16:12:45 +0900

    Deploy 1.0

--- FinderPlusPlus/AppDelegate.swift
+++ FinderPlusPlus/AppDelegate.swift
@@ -11,10 +11,47 @@ import Cocoa
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
+    var preferencesWindowController:NSWindowController?
 
-    @IBOutlet weak var window: NSWindow!
-    
+    // MARK: Delegates
+
     func applicationDidFinishLaunching(aNotification: NSNotification) {
+        self.initPreferencesWindow()
+    }
+
+    func applicationDidBecomeActive(notification: NSNotification) {
+        self.reopenPreferencesWindow()
+    }
+
+    func applicationDidResignActive(notification: NSNotification) {
+        self.hidePreferencesWindow()
+    }
+
+    // MARK: - Actions
+
+    @IBAction func onClickPreferences(sender: AnyObject) {
+        reopenPreferencesWindow()
+    }
+
+    // MARK: - Private functions
+
+    private func initPreferencesWindow() {
+        self.preferencesWindowController = NSWindowController(windowNibName: "SettingWindow")
+        self.preferencesWindowController!.showWindow(self)
+        self.preferencesWindowController!.window?.makeMainWindow()
+    }
+
+    private func reopenPreferencesWindow() {
+        if self.preferencesWindowController!.windowLoaded {
+            self.preferencesWindowController!.window?.makeKeyAndOrderFront(self)
+        }
+        else {
+            self.preferencesWindowController!.showWindow(self)
+            self.preferencesWindowController!.window?.makeMainWindow()
+        }
+    }
+
+    private func hidePreferencesWindow() {
+        self.preferencesWindowController!.window?.orderOut(nil)
     }
 }
-
